364bd3a5a90075f09d741456b4a8a818a0ca0b22
shar <faethychan@gmail.com>
2023-12-01 18:57:12 +0800

    Add spheres random position spawning

---
+++ Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour
+{
+    [SerializeField]private GameObject[] objectsToSpawn = default;
+    [SerializeField]private Camera camera = default;
+    private float height;
+    private float width;
+    
+    private void Awake() {
+        height = 2f * camera.orthographicSize;
+        width = height * camera.aspect;
+
+        height /= 2;
+        width /= 2;
+        height -= 0.5f;
+        width -= 0.5f;
+    }
+
+    private void Start() {
+        InvokeRepeating("SpawnObject", 1.0f, 2.0f);
+    }
+
+    private void SpawnObject() {
+        var randomElement = Random.Range(0, objectsToSpawn.Length);
+        var randomXPosition = Random.Range(width * -1, width);
+        var randomYPosition = Random.Range(height * -1, height);
+        var randomPosition = new Vector3(randomXPosition, randomYPosition, 0);
+
+        var spawnedObject = Instantiate(objectsToSpawn[randomElement], randomPosition, Quaternion.identity);  
+        Destroy(spawnedObject, 3);  
+    }
+}
