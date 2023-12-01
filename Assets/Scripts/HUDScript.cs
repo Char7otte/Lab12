b7a816008492790c47522d24c0a592380e0ace6c
shar <faethychan@gmail.com>
2023-12-01 19:56:42 +0800

    Added a new script for updating the hud

---
+++ Assets/Scripts/HUDScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HUDScript : MonoBehaviour
+{
+    [SerializeField]private TextMeshProUGUI scoreTextDisplay = default;
+    [SerializeField]private TextMeshProUGUI timeTextDisplay = default;
+
+    private void Update() {
+        var gameManager = GameManager.instance;
+        scoreTextDisplay.text = "Score: " + gameManager.scoreValue;
+        timeTextDisplay.text = "Time: " + gameManager.timeValue.ToString("F0");
+    }
+}
