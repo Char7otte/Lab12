15522c6010841c3323ceb77d46b31e211fd5f998
shar <faethychan@gmail.com>
2023-12-01 19:37:38 +0800

    Add GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public int scoreValue;
+    public float timeValue;
+
+    private void Awake() {
+        if (instance != null) Destroy(this);
+        else instance = this;
+    }
+}
