c96b5f2eed4a5cdb0a03e19893f600d8cc04f0e4
shar <faethychan@gmail.com>
2023-12-01 19:26:11 +0800

    Add winning and losing conditions

--- Assets/Scripts/SpawnScript.cs
+++ Assets/Scripts/SpawnScript.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class SpawnScript : MonoBehaviour
 {
@@ -12,6 +13,9 @@ public class SpawnScript : MonoBehaviour
 
     private int scoreValue = default;
     [SerializeField]private TextMeshProUGUI scoreTextDisplay = default;
+
+    private float timeValue;
+    [SerializeField]private TextMeshProUGUI timeTextDisplay = default;
     
     private void Awake() {
         mainCamera = Camera.main;
@@ -29,20 +33,21 @@ public class SpawnScript : MonoBehaviour
     }
 
     private void Update() {
-        RaycastScript();
+        MouseRaycast();
+        CountUpTimer();
     }
 
     private void SpawnObject() {
         var randomElement = Random.Range(0, objectsToSpawn.Length);
-        var randomXPosition = Random.Range(width * -1, width);
-        var randomYPosition = Random.Range(height * -1, height);
-        var randomPosition = new Vector3(randomXPosition, randomYPosition, 0);
+        var randomPositionX = Random.Range(width * -1, width);
+        var randomPositionY = Random.Range(height * -1, height);
+        var randomPosition = new Vector3(randomPositionX, randomPositionY, 0);
 
         var spawnedObject = Instantiate(objectsToSpawn[randomElement], randomPosition, Quaternion.identity);  
         Destroy(spawnedObject, 3);  
     }
 
-    private void RaycastScript() {
+    private void MouseRaycast() {
         if (Input.GetMouseButtonDown(0)) {
             var worldMousePosition = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 100f));
             var direction = worldMousePosition - mainCamera.transform.position;
@@ -64,10 +69,22 @@ public class SpawnScript : MonoBehaviour
                 Destroy(_hit);
                 scoreValue += pointGain;
                 scoreTextDisplay.text = "Score: " + scoreValue;
+
+                if (scoreValue >= 15) ChangeScene("GameWin");
+
             }
-            else {
-                Debug.DrawLine(mainCamera.transform.position, worldMousePosition, Color.red, 0.5f);
-            }
+            else Debug.DrawLine(mainCamera.transform.position, worldMousePosition, Color.red, 0.5f);
         }
+    }
+
+    private void CountUpTimer() {
+        timeValue += Time.deltaTime;
+        timeTextDisplay.text = "Timer: " + timeValue.ToString("F0");
+
+        if (timeValue >= 10) ChangeScene("GameLose");
+    }
+
+    private void ChangeScene(string newSceneName) {
+        SceneManager.LoadScene(newSceneName);
     }
 }
